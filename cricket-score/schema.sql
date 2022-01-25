26efffcb0b8e3c97dc1656a25b7d28e93056feb5
john-baxter <john.g.baxter@googlemail.com>
2022-01-25 21:05:14 +0000

    Add file schema.sql and populate with first two tables  and .

---
+++ cricket-score/schema.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTSpost;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+CREATE TABLE match_info (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  scorer_id INTEGER NOT NULL,
+  date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  -- TODO
+  -- Find out how to change the date so that it is just the date (without time)
+  venue TEXT,
+  team_a TEXT NOT NULL,
+  team_b TEXT NOT NULL,
+  FOREIGN KEY (scorer_id) REFERENCES user (id)
+);
